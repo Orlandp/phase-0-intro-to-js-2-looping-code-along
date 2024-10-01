6ac5cd7b5312f38f0f7c76bef04105ecc224b5af
pknowledge <jamesblssings@gmail.com>
2024-10-01 06:22:15 +0300

    for and while loops

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+// Example usage:
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+// Output: 
+// [
+//   "Thank you, Charlie, for the wonderful birthday gift!",
+//   "Thank you, Samip, for the wonderful birthday gift!",
+//   "Thank you, Ali, for the wonderful birthday gift!"
+// ]
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+
+// Example usage:
+countDown(10);
+// Output:
+// 10
+// 9
+// 8
+// 7
+// 6
+// 5
+// 4
+// 3
+// 2
+// 1
+// 0
+
